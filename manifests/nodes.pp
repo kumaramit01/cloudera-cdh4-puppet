cd9dcda3a03f09081bd03678be474c5db0812cfd
Robb Kidd <robb@thekidds.org>
2012-09-13 13:39:17 -0400

    Remove HBase for the moment. May add back in later.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,12 +1,9 @@
 node /slave\d+/ {
   include hadoop::datanode
   include hadoop::tasktracker
-  include hbase::regionserver
-
 }
 
 node master {
   include hadoop::namenode
   include hadoop::jobtracker
-  include hbase::master
 }
